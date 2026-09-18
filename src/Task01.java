3f07947a35743dfb8f92385c9c3e96697c3c45bf
shlouie1 <carterwiesnerofficial@gmail.com>
2026-09-18 12:44:28 -0400

    Completed part 1

---
+++ src/Task01.java
@@ -0,0 +1,62 @@
+public class Task01 {
+    public void main() {
+        // intOperandA, intOperandB, intSum, intProduct, intDifference, intQuotient, intModulo
+
+        int intOperandA = 1;
+        int intOperandB = 13;
+        int intSum = 10;
+        int intProduct = 34;
+        int intDifference = 0;
+        int intQuotient = 892;
+        int intModulo = 12;
+
+        double doubleOperandA = 2.50;
+        double doubleOperandB = 23.12;
+        double doubleSum = 98.21;
+        double doubleProduct = 32.123;
+        double doubleDifference = 849.239;
+        double doubleQuotient = 257.93;
+
+        intSum = intOperandA + intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
+        System.out.println("The difference using ints of " + intOperandA + " " + intOperandB + " is " + intDifference);
+        System.out.println("The product using ints of " + intOperandA + " " + intOperandB + " is " + intProduct);
+        System.out.println("The quotient using ints of " + intOperandA + " " + intOperandB + " is " + intQuotient);
+        System.out.println("The modulo using ints of " + intOperandA + " " + intOperandB + " is " + intModulo);
+
+        System.out.println("\nThe sum using doubles of " + intOperandA + " & " + intOperandB + " is " + doubleSum);
+        System.out.println("The difference using doubles of " + intOperandA + " & " + intOperandB + " is " + doubleDifference);
+        System.out.println("The product using doubles of " + intOperandA + " & " + intOperandB + " is " + doubleProduct);
+        System.out.println("The quotient using doubles of " + intOperandA + " & " + intOperandB + " is " + doubleQuotient);
+
+        System.out.println("\n*********\n");
+
+        double myLunchCost = 10.50;
+        int numberOfKidsInMyFamily = 2;
+        boolean isRainingStatus = false;
+        int myFavNum = 84;
+        double myShoeSize = 12.5;
+        String myBirthMonth = "May";
+        String myFullName = "Carter Jackson Wiesner";
+
+        System.out.println("The cost of my lunch is " + myLunchCost);
+        System.out.println("The number of kids in my family is " + numberOfKidsInMyFamily);
+        System.out.println("Is it raining: " + isRainingStatus);
+        System.out.println("My favorite number is " + myFavNum);
+        System.out.println("I wear a shoe size of " + myShoeSize);
+        System.out.println("I was born in the month of " + myBirthMonth);
+        System.out.println("Hi, my full name is " + myFullName);
+
+
+    }
+}
---
+++ src/Task02.java
@@ -0,0 +1,5 @@
+public class Task02 {
+    public void main() {
+
+    }
+}
